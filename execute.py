48de54292e1f322d375606a178af957489745de7
TieKotek <1603700364@qq.com>
2022-11-02 05:10:37 +0000

    finish execute.py

---
+++ execute.py
@@ -0,0 +1,38 @@
+import subprocess
+import json
+from config import *
+from hashlib import sha256
+
+
+def execute(file_path, sample_list):
+    output_list = []
+    compile_result = subprocess.run(['g++', file_path], text=True, shell=True, stderr=subprocess.DEVNULL)
+    for sample in sample_list:
+        output = {}
+        if compile_result.returncode == 0:
+            try:
+                run_result = subprocess.run(['./a'], text=True, input=sample, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=timeout)
+                if run_result.returncode == 0:
+                    output["status"] = "AC"
+                    output["output"] = run_result.stdout
+                    output["error"] = run_result.stderr
+                else: 
+                    output["status"] = "RE"
+                    output["output"] = run_result.stdout
+                    output["error"] = run_result.stderr
+            except subprocess.TimeoutExpired:
+                output["status"] = "TLE"
+                output["output"] = ""
+                output["error"] = ""
+        else: 
+            output["status"] = "CE"
+            output["output"] = ""
+            output["error"] = ""
+        output_list.append(output)
+    f_json = {}
+    f_json["name"] = file_path
+    output_str = json.dumps(output_list)
+    if use_hash: 
+        f_json["output"] = sha256(output_str.encode('utf-8')).hexdigest()
+    else: f_json["output"] = output_str
+    return f_json
